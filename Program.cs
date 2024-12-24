b053ad040333b76b45eff884bef9120b68ecf4bb
kingmades <mades.almeida@outlook.com>
2024-12-24 16:15:50 -0300

    add a start

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Stopwatch
 {
-    class Program
-    {
-    	static void Main(string[] args)
-        {
-            Console.WriteLine("Stop Watch");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Start();
+		}
+
+		static void Start()
+		{
+			int time = 10;
+			int currentTime = 0;
+
+			while (currentTime != time)
+			{
+				Console.Clear();
+				currentTime++;
+				Console.WriteLine(currentTime);
+			}
+		}
+	}
 }
