95180e5c952e68b0a5b819e8098c8f8d7ddfccca
kingmades <mades.almeida@outlook.com>
2024-12-24 17:07:16 -0300

    add menu

--- Program.cs
+++ Program.cs
@@ -4,12 +4,44 @@
 	{
 		static void Main(string[] args)
 		{
-			Start();
+			Menu();
 		}
 
-		static void Start()
+		static void Menu()
 		{
-			int time = 10;
+			Console.Clear();
+			Console.WriteLine("S = Segundo - 10s = 10 segundos");
+			Console.WriteLine("M = Minuto - 1m = q minuto");
+			Console.WriteLine("0 = Sair");
+			Console.WriteLine("Quanto tempo deseja contar?");
+
+			string data = Console.ReadLine().ToLower();
+			char type = char.Parse(data.Substring(data.Length - 1, 1));
+			int time = int.Parse(data.Substring(0, data.Length - 1));
+
+			int multiplier = 1;
+
+			if (type == 'm') multiplier = 60;
+			if (type == 0) Environment.Exit(0);
+
+			PreStart(time * multiplier);
+		}
+
+		static void PreStart(int time)
+		{
+			Console.Clear();
+			Console.WriteLine("Ready...");
+			Thread.Sleep(1000);
+			Console.WriteLine("Set...");
+			Thread.Sleep(1000);
+			Console.WriteLine("Go...");
+			Thread.Sleep(2500);
+
+			Start(time);
+		}
+
+		static void Start(int time)
+		{
 			int currentTime = 0;
 
 			while (currentTime != time)
@@ -17,7 +49,13 @@
 				Console.Clear();
 				currentTime++;
 				Console.WriteLine(currentTime);
+				Thread.Sleep(1000);
 			}
+
+			Console.Clear();
+			Console.WriteLine("Stopwatch finalizado");
+			Thread.Sleep(2500);
+			Menu();
 		}
 	}
 }
